8afb582a5252cfe52295b2c10ef3db1633c2b417
Victor <victor.fiamoncini@gmail.com>
2022-11-05 22:12:46 -0300

    feat: add home-page loading and error UI

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:atv_final_flutter_mobile/domain/entities/address_entity.dart';
 import 'package:atv_final_flutter_mobile/domain/usecases/fetch_address_use_case.dart';
 import 'package:atv_final_flutter_mobile/domain/usecases/fetch_location_use_case.dart';
+import 'package:atv_final_flutter_mobile/ui/widgets/background_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   final FetchLocationUseCase fetchLocationUseCase;
@@ -20,21 +22,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Future<AddressEntity> _getUserAddress() async {
-    try {
-      final position = await widget.fetchLocationUseCase.fetchLocation();
-      final address = await widget.fetchAddressUseCase.fetchAddress(
-        FetchAddressUseCaseParams(
-          latitude: position.latitude,
-          longitude: position.longitude,
-        ),
-      );
+    final position = await widget.fetchLocationUseCase.fetchLocation();
+    final address = await widget.fetchAddressUseCase.fetchAddress(
+      FetchAddressUseCaseParams(
+        latitude: position.latitude,
+        longitude: position.longitude,
+      ),
+    );
 
-      return address;
-    } catch (e) {
-      print('ERRRRRRRRRRRRRRRRRROU');
-      print(e);
-      throw Exception('');
-    }
+    return address;
+  }
+
+  Widget _buildErrorMessage(ThemeData theme) {
+    return Center(
+      child: Text(
+        'Houve um erro ao buscar as informações, tente novamente em breve',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: theme.colorScheme.primary,
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoading(ThemeData theme) {
+    return SpinKitThreeBounce(
+      color: theme.colorScheme.primary,
+      size: 76,
+    );
   }
 
   @override
@@ -44,39 +61,70 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          height: mediaQuery.size.height,
-          width: mediaQuery.size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: const AssetImage('assets/main-background.png'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                theme.colorScheme.primary.withOpacity(0.68),
-                BlendMode.dstATop,
-              ),
-            ),
-          ),
+        child: BackgroundWrapper(
           child: Center(
-            child: FutureBuilder(
-              future: _getUserAddress(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error');
-                }
+            child: Container(
+              width: mediaQuery.size.width,
+              constraints: BoxConstraints(
+                maxHeight: 400,
+                minHeight: 380,
+                minWidth: mediaQuery.size.width,
+              ),
+              child: Card(
+                elevation: 50,
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(12),
+                  child: FutureBuilder(
+                    future: _getUserAddress(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) return _buildErrorMessage(theme);
 
-                if (snapshot.hasData) {
-                  print(snapshot.data?.country);
+                      if (snapshot.hasData) {
+                        final city = snapshot.data?.city ?? '';
+                        final date = DateFormat(DateFormat.YEAR_MONTH).format(
+                          DateTime.now(),
+                        );
 
-                  return Text(snapshot.data?.city as String);
-                }
+                        return Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  city,
+                                  style: TextStyle(
+                                    color: theme.colorScheme.primary,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 2),
+                            Row(
+                              children: [
+                                Text(
+                                  date,
+                                  style: TextStyle(
+                                    color: theme.colorScheme.primary,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        );
+                      }
 
-                return SpinKitThreeBounce(
-                  color: theme.colorScheme.primary,
-                  size: 82,
-                );
-              },
+                      return _buildLoading(theme);
+                    },
+                  ),
+                ),
+              ),
             ),
           ),
         ),
